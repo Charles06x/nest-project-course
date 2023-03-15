34267ba3b5c1aa958fe6163c34ec352abc1d8644
Charles06x <29361605+Charles06x@users.noreply.github.com>
2023-03-15 11:36:03 -0500

    ✏️ Adjust strings responses

--- src/dog/services/dog/dog.service.ts
+++ src/dog/services/dog/dog.service.ts
@@ -7,18 +7,22 @@ export class DogService {
   }
 
   findOne(dogId: number): string {
-    return `findOne() is working and the dogID is ${dogId}`;
+    return `findOne is working, and the dogID is ${dogId}`;
   }
 
   create(breed: string, age: number, color: string) {
-    return `Create function with properties breed: ${breed}, age: ${age} &  color: ${color}`;
+    return `create function with properties, breed: ${breed}, age: ${age} &  color: ${color}`;
   }
 
   update(id: number, breed: string, age: number, color: string) {
-    return `Update function with properties id: ${id}, breed: ${breed}, age: ${age} &  color: ${color}`;
+    let properties = '';
+    properties = breed ? properties + `breed: ${breed}` : properties;
+    properties = age ? properties + `age: ${age}` : properties;
+    properties = color ? properties + `color: ${color}` : properties;
+    return `update function dog with id: ${id}, ${properties}`;
   }
 
   delete(id) {
-    return `Delete function working with id: ${id}`;
+    return `delete function dog with id: ${id}`;
   }
 }

10d1a931d9cb4038c8cee91e3e10d4c5cb0b22ac
Charles06x <29361605+Charles06x@users.noreply.github.com>
2023-03-15 15:06:52 -0500

    Use the datasource to return valid info

--- src/dog/services/dog/dog.service.ts
+++ src/dog/services/dog/dog.service.ts
@@ -1,28 +1,74 @@
 import { Injectable } from '@nestjs/common';
+import { DOGS } from 'datasource/dogs';
 
 @Injectable()
 export class DogService {
-  find(breed: string, age: number): string {
-    return `find function with params breed: ${breed} & age: ${age}`
+
+  findAll() {
+    return DOGS;
   }
 
-  findOne(dogId: number): string {
-    return `findOne is working, and the dogID is ${dogId}`;
+  find(breed: string, age: number): any[] {
+    let filteredDogs = [];
+
+    if(breed && age) {
+      return DOGS.filter((dog) => dog.breed == breed && dog.age >= age);
+    } 
+
+    breed
+      ? (filteredDogs = DOGS.filter((dog) => dog.breed == breed))
+      : (filteredDogs = DOGS.filter((dog) => dog.age >= age));
+
+    return filteredDogs;
   }
+
+  findOne(dogId: number): any {
+
+    try {
+      const [dog] = DOGS.filter((dog) => Number(dog.id) === Number(dogId));
+  
+      return dog;
+    } catch (error) {
+      return {
+        msg: 'Something went wrong. Try later.'
+      }
+    }
+  }
+
 
   create(breed: string, age: number, color: string) {
-    return `create function with properties, breed: ${breed}, age: ${age} &  color: ${color}`;
+
+    const newDog = {
+      id: DOGS.length,
+      breed,
+      age,
+      color,
+    }
+
+    DOGS.push(newDog);
+
+    return DOGS[DOGS.length - 1];
   }
 
   update(id: number, breed: string, age: number, color: string) {
-    let properties = '';
-    properties = breed ? properties + `breed: ${breed}` : properties;
-    properties = age ? properties + `age: ${age}` : properties;
-    properties = color ? properties + `color: ${color}` : properties;
-    return `update function dog with id: ${id}, ${properties}`;
+    
+    const dogToUpdateIndex = DOGS.findIndex(dog => Number(dog.id === Number(id)))
+
+    const currentDogInfo = DOGS[dogToUpdateIndex]
+    DOGS[dogToUpdateIndex] = {
+      ...DOGS[dogToUpdateIndex],
+      breed: breed ?? currentDogInfo.breed ,
+      age: age ?? currentDogInfo.age,
+      color: color ?? currentDogInfo.color,
+    }
+
+    return DOGS[dogToUpdateIndex]
   }
 
-  delete(id) {
-    return `delete function dog with id: ${id}`;
+  delete(id: number) {
+    const dogToDeleteIndex = DOGS.findIndex(dog => Number(dog.id === Number(id)))
+    DOGS.splice(dogToDeleteIndex, 1);
+
+    return 'Dog deleted successfully';
   }
 }
